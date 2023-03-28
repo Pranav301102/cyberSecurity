73dd298f5ab489c2d15d11ef6f1d41753c1a09f7
Pranav <pranavtrivedi02@gmail.com>
2023-03-28 19:22:44 +0530

    updated UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+
 import './App.css';
-import Header from './Header';
+import Background from './component/background';
+import Home from './component/home';
+import Domain from './component/domain';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-
-    <div className="app">      
-      <Header />
+    <div className="App">
+      <Router>
+        <Background/>
+          <Routes>
+          <Route exact path="/" element={<Home />} />
+          </Routes>
+      </Router>
     </div>
-
   );
 }
 
 export default App;
+
+
+
